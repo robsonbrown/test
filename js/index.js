7c058cda5754ee56305d1acbd0a05a4aaee102e6
robsonbrown <git@dixonbrown.com>
2013-08-06 09:01:21 +0100

    Further cleanup to the login jquery

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 	  
+	  //-------------------
+	  // Login functionality
+	  //--------------------
 	  $( "#login" )
       .button()
-      .click(function() {
+      .click(function() 
+	  {
 		var header = "login";
 		var myusername = $( "#myusername" ).val();
 		var	mypassword = $( "#mypassword" ).val();
@@ -19,29 +23,28 @@
 			}
 			else if( result == 'passed' )
 			{
-				alert( result );
+				location.reload();
 			}
 			else
 			{
 				alert( result );
-				//location.reload();
 			}
-		
-		
-			//alert("hello");
-			//$("#msg").html( " Address of Roll no " +myusername +" is "+mypassword );
 		});
 	   });
 	  
-	  
-	    $( "#create-user" )
+	  //-------------------
+	  // Create user dialog opening button
+	  //--------------------
+	  $( "#create-user" )
       .button()
-      .click(function() {
-        $( "#dialog-form" ).dialog( "open" );
+      .click(function() 
+	  {
+			$( "#dialog-form" ).dialog( "open" );
       });
 	  
-	  
-	  
+	  //-------------------
+	  // User dialog
+	  //--------------------
 	  $(function() 
 	  {
 		var nameBox 	= $( "#name" ),
@@ -63,53 +66,49 @@
 		{
 			"Create an account": function() 
 			{
-			var bValid = true;
-			
-			alert("Create account called");
-			
-			var 	header = "signUp";
-			var 	myusername = $( "#username" ).val();
-			var 	name = $( "#name" ).val();
-			var		password = $( "#password" ).val();
-			var 	finance = 0;
-			var		email = $( "#email" ).val();
-			
-			//allFields.removeClass( "ui-state-error" );
-	
-			bValid = bValid && checkLength( nameBox, "name", 3, 16 );
-			 bValid = bValid && checkLength( username, "username", 3, 16 );
-			 bValid = bValid && checkLength( email, "email", 6, 80 );
-			 bValid = bValid && checkLength( password, "password", 5, 16 );
-			
-			 bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Name may consist of a-z, 0-9, underscores, begin with a letter." );
-			 bValid = bValid && checkRegexp( username, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
-			 bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
-			 bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
-	
-			if ( bValid ) 
-			{		  
-				$.ajax({
-				type: "POST",
-				url: "session/session_handler.php",
-				data: {head:header, username:myusername, name:name, password:password, finance:finance, email:email}}
-				).done
-				(
-					function( result ) 
-					{
-						alert("hello");
-						alert(result);
-					}
-				);
+				var bValid = true;
 				
-				alert( bValid );
-				$( this ).dialog( "close" );
+				var 	header = "signUp";
+				var 	myusername = $( "#username" ).val();
+				var 	name = $( "#name" ).val();
+				var		password = $( "#password" ).val();
+				var 	finance = 0;
+				var		email = $( "#email" ).val();
+				
+				allFields.removeClass( "ui-state-error" );
+		
+				bValid = bValid && checkLength( nameBox, "name", 3, 16 );
+				bValid = bValid && checkLength( usernameBox, "username", 3, 16 );
+				bValid = bValid && checkLength( emailBox, "email", 6, 80 );
+				bValid = bValid && checkLength( passwordBox, "password", 5, 16 );
+				
+				bValid = bValid && checkRegexp( nameBox, /^[a-z]([0-9a-z_])+$/i, "Name may consist of a-z, 0-9, underscores, begin with a letter." );
+				bValid = bValid && checkRegexp( usernameBox, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
+				bValid = bValid && checkRegexp( emailBox, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
+				bValid = bValid && checkRegexp( passwordBox, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+		
+				if ( bValid ) 
+				{		  
+					$.ajax({
+					type: "POST",
+					url: "session/session_handler.php",
+					data: {head:header, username:myusername, name:name, password:password, finance:finance, email:email}}
+					).done
+					(
+						function( result ) 
+						{
+							location.reload();
+						}
+					);
+					
+					$( this ).dialog( "close" );
 				}
-				},
-				Cancel: function() 
-				{
+			},
+			Cancel: function() 
+			{
 				$( this ).dialog( "close" );
-				alert("cancelled");
-				}
+				//alert("cancelled");
+			}
 		},
 		close: function() 
 		{
@@ -160,8 +159,6 @@
 			tips.removeClass( "ui-state-highlight", 1500 );
 		 }, 500 );
 		}
-	
-	    
   });
 	
 	
